1fd7f1a3d0cd5cd0c15b22374a6bc13cdf0a38c9
Delton Idah <idahdelton@gmail.com>
2020-10-09 22:49:18 +0300

    ggplot commit

--- ggplot2.R
+++ ggplot2.R
@@ -7,5 +7,10 @@ library(tidyverse)
 #Data
 mpg
 # Creating a ggplot
+# displ=engine size;hwy=fuelefficiency 
 ggplot(data = mpg)+
   geom_point(mapping = aes(x = displ,y = hwy))
+# Adding an aesthetic to the code
+# An example in this case is the class of the car
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
